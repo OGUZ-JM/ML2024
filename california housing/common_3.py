4e6dab587b595018c2a55fd08079420968d6f543
OGUZ-JM <mathias95400@hotmail.fr>
2024-12-21 08:49:39 +0100

    Cours classification

--- california housing/common_3.py
+++ california housing/common_3.py
@@ -8,6 +8,12 @@ def sets_creation(df, first_threshold, ratio):
 
     training_set = df[:training_set_threshold]
     test_set = df[training_set_threshold:]
+
+    #separe le y (variable a predire) des x (variables explicatives)
+
+    X = test_set.drop(columns=['median_house_value'])
+    Y = test_set['median_house_value']
+
     print("training_set size", len(training_set))
     print("test_set size", len(test_set))
     
